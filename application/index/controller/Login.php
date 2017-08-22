dcee8e85b23ffc16c72f4997731dc5d04da2807b
LeonaGan <947698259@qq.com>
2017-08-22 15:19:59 +0800

    修改返回前端页面的形式

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -10,17 +10,24 @@ use think\Session;
 
 class Login extends Controller{
     public function index(){
-        $view = new View();
         if(Session::get('user_id')){//用户已经登陆，直接进入用户中心
-        $user_id=Session::get('user_id');
-        $user_model = new UserModel();//实例化用户模型
-          if($user_model->isManager($user_id)){//管理员
-              return $view->fetch('usermanage/user_manage');
-          }else{//普通用户
-              return $view->fetch('userindex/user_index');
-          }
+            $user_id=Session::get('user_id');
+            $user_model = new UserModel();//实例化用户模型
+            if($user_model->isManager($user_id)){//管理员
+                return $this->fetch('usermanage/user_manage');
+            }else{//普通用户
+                $user_model_id = new UserModel();
+                $user_data=$user_model_id->getMessageById($user_id);
+                $event_model = new EventModel();
+                $event_data=$event_model->getEventMessageByUser($user_id);
+                $result_data=array();
+                $result_data['user_message']=$user_data;
+                $result_data['event_message']=$event_data;
+                $this->assign('data',$result_data);
+                return $this->fetch('userindex/user_index');
+            }
         }else{
-            return $view->fetch('login/index');//用户未登录，进入登陆界面
+            return $this->fetch('login/index');//用户未登录，进入登陆界面
         }
     }
 
