70fec43feadbd4be76ad92a198562268560d2116
LeonaGan <947698259@qq.com>
2017-08-23 09:38:57 +0800

    用户管理界面，增加后台更新

--- application/index/model/EventModel.php
+++ application/index/model/EventModel.php
@@ -10,11 +10,10 @@ class EventModel extends Model{
     //获取用户信息
     public function getEventMessageByUser($user_id){
       //$result="";
-      $result = Db::query("select event_title,event_description,event_time,participant_num,event_num,item_name from event, participant,item where participant.refer_event_id=event.event_id and event.event_id=item.refer_event_id");
+      $result = Db::query("select event_id,event_title,event_description,participant_num,event_num,item_name from event, participant,item where participant.refer_event_id=event.event_id and event.event_id=item.refer_event_id");
       if (empty($result)) {//获取数据，若不存在则返回空
           return null;
       }
-       Log::info($result);
       //筛选在有效时间内的事件
       return $result;   //返回用户信息
 
