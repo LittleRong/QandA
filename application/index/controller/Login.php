3bedad71ea08f0e49db6694663d0243d352a3c1e
LeonaGan <947698259@qq.com>
2017-09-04 22:50:36 +0800

    fixbug,1.普通用户修改密码界面：在提示原密码输入错误或修改成功的情况下，跳转到登录界面;2.普通用户修改密码界面：在提示两次密码不一样的情况下，点击确定后确认按钮不可用

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -11,25 +11,25 @@ use think\Session;
 
 class Login extends Controller{
     public function index(){
-        // if(Session::get('user_id')){//用户已经登陆，直接进入用户中心
-        //     $user_id=Session::get('user_id');
-        //     $user_model = new UserModel();//实例化用户模型
-        //     if($user_model->isManager($user_id)){//管理员
-        //         return $this->fetch('usermanage/user_manage');
-        //     }else{//普通用户
-        //         $user_model_id = new UserModel();
-        //         $user_data=$user_model_id->getMessageById($user_id);
-        //         $event_model = new EventModel();
-        //         $event_data=$event_model->getEventMessageByUser($user_id);
-        //         $result_data=array();
-        //         $result_data['user_message']=$user_data;
-        //         $result_data['event_message']=$event_data;
-        //         $this->assign('data',$result_data);
-        //         return $this->fetch('userindex/user_index');
-        //     }
-        // }else{
+        if(Session::get('user_id')){//用户已经登陆，直接进入用户中心
+            $user_id=Session::get('user_id');
+            $user_model = new UserModel();//实例化用户模型
+            if($user_model->isManager($user_id)){//管理员
+                return $this->fetch('usermanage/user_manage');
+            }else{//普通用户
+                $user_model_id = new UserModel();
+                $user_data=$user_model_id->getMessageById($user_id);
+                $event_model = new EventModel();
+                $event_data=$event_model->getEventMessageByUser($user_id);
+                $result_data=array();
+                $result_data['user_message']=$user_data;
+                $result_data['event_message']=$event_data;
+                $this->assign('data',$result_data);
+                return $this->fetch('userindex/user_index');
+            }
+        }else{
             return $this->fetch('login/index');//用户未登录，进入登陆界面
-        // }
+        }
     }
 
     //验证登陆
